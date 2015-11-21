6fe40175565b29b7960812473c7ace831b1629fb
Matthew Murphy <matthew.e.murphy@phila.gov>
2015-11-21 15:37:04 -0500

    Adding data for title combo.

--- site/views/register/view.html.php
+++ site/views/register/view.html.php
@@ -22,122 +22,129 @@ jimport('joomla.application.component.view');
  *
  * @since 1.0
  */
-class PvmachineinspectorsViewRegister extends JView
-{
-    public function display()
-    {
-        global $mainframe;
-        $pathway  = &$mainframe->getPathway();
-        $document = &JFactory::getDocument();
-        $params   = &$mainframe->getParams();
+class PvmachineinspectorsViewRegister extends JView {
+	public function display() {
+		global $mainframe;
+		$pathway  = &$mainframe->getPathway();
+		$document = &JFactory::getDocument();
+		$params   = &$mainframe->getParams();
 
-        // Page Title
-        $menus = &JSite::getMenu();
-        $menu  = $menus->getActive();
+		// Page Title
+		$menus = &JSite::getMenu();
+		$menu  = $menus->getActive();
 
-        // fallback title
-        $title = 'Machine Inspector Application';
+		// fallback title
+		$title = 'Machine Inspector Application';
 
-        if (is_object($menu)) {
-            $menu_params = new JParameter($menu->params);
-            if (!$menu_params->get('page_title')) {
-                $params->set('page_title', JText::_($title));
-            }
-        } else {
-            $params->set('page_title', JText::_($title));
-        }
+		if (is_object($menu)) {
+			$menu_params = new JParameter($menu->params);
+			if (!$menu_params->get('page_title')) {
+				$params->set('page_title', JText::_($title));
+			}
+		} else {
+			$params->set('page_title', JText::_($title));
+		}
 
-        // data for select elements
-        $db = &JFactory::getDBO();
-        $db->setQuery('SELECT DISTINCT `ward` FROM `#__divisions` ORDER BY left(`division_id`, 2) ASC');
-        if (!($wards = $db->loadObjectList())) {
-            $this->setError(JText::_('Wards query failed'));
-        }
-        $db->setQuery('SELECT `id`, `division_id`, `ward`, `division` FROM `#__divisions` ORDER BY `division_id` ASC');
-        if (!($divisions = $db->loadObjectList())) {
-            $this->setError(JText::_('Divisions query failed'));
-        }
+		// data for select elements
+		$db = &JFactory::getDBO();
+		$db->setQuery('SELECT DISTINCT `ward` FROM `#__divisions` ORDER BY left(`division_id`, 2) ASC');
+		if (!($wards = $db->loadObjectList())) {
+			$this->setError(JText::_('Wards query failed'));
+		}
+		$db->setQuery('SELECT `id`, `division_id`, `ward`, `division` FROM `#__divisions` ORDER BY `division_id` ASC');
+		if (!($divisions = $db->loadObjectList())) {
+			$this->setError(JText::_('Divisions query failed'));
+		}
 
-        //
-        $document->setTitle($params->get('page_title'));
-        $this->assignRef('params', $params);
-        $this->assignRef('wards', $wards);
-        $this->assignRef('divisions', $divisions);
+		//
+		$document->setTitle($params->get('page_title'));
+		$this->assignRef('params', $params);
+		$this->assignRef('wards', $wards);
+		$this->assignRef('divisions', $divisions);
 
-        //        $pathway->addItem( JText::_( 'New' ));
+		//        $pathway->addItem( JText::_( 'New' ));
 
-        // Load the form validation behavior
-        JHTML::_('behavior.formvalidation');
+		// Load the form validation behavior
+		JHTML::_('behavior.formvalidation');
 
-        parent::display();
-    }
+		parent::display();
+	}
 
-    public function getUSStates()
-    {
-        $states = array(
-            'AL' => 'Alabama',
-            'AK' => 'Alaska',
-            'AS' => 'American',
-            'AZ' => 'Arizona',
-            'AR' => 'Arkansas',
-            'CA' => 'California',
-            'CO' => 'Colorado',
-            'CT' => 'Connecticut',
-            'DE' => 'Delaware',
-            'DC' => 'Dist. of Columbia',
-            'FL' => 'Florida',
-            'GA' => 'Georgia',
-            'GU' => 'Guam',
-            'HI' => 'Hawaii',
-            'ID' => 'Idaho',
-            'IL' => 'Illinois',
-            'IN' => 'Indiana',
-            'IA' => 'Iowa',
-            'KS' => 'Kansas',
-            'KY' => 'Kentucky',
-            'LA' => 'Louisiana',
-            'ME' => 'Maine',
-            'MD' => 'Maryland',
-            'MH' => 'Marshall',
-            'MA' => 'Massachusetts',
-            'MI' => 'Michigan',
-            'FM' => 'Micronesia',
-            'MN' => 'Minnesota',
-            'MS' => 'Mississippi',
-            'MO' => 'Missouri',
-            'MT' => 'Montana',
-            'NE' => 'Nebraska',
-            'NV' => 'Nevada',
-            'NH' => 'New Hampshire',
-            'NJ' => 'New Jersey',
-            'NM' => 'New Mexico',
-            'NY' => 'New York',
-            'NC' => 'North Carolina',
-            'ND' => 'North Dakota',
-            'MP' => 'Northern',
-            'OH' => 'Ohio',
-            'OK' => 'Oklahoma',
-            'OR' => 'Oregon',
-            'PW' => 'Palau',
-            'PA' => 'Pennsylvania',
-            'PR' => 'Puerto Rico',
-            'RI' => 'Rhode Island',
-            'SC' => 'South Carolina',
-            'SD' => 'South Dakota',
-            'TN' => 'Tennessee',
-            'TX' => 'Texas',
-            'UT' => 'Utah',
-            'VT' => 'Vermont',
-            'VA' => 'Virginia',
-            'VI' => 'Virgin Islands',
-            'WA' => 'Washington',
-            'WV' => 'West Virginia',
-            'WI' => 'Wisconsin',
-            'WY' => 'Wyoming');
+	public function getTitles() {
+		$arr = array(
+			'Mr.', 'Mrs.', 'Miss', 'Dr.',
+		);
 
-        foreach ($states as $abbr => $state) {
-            $return[] = (object) array('abbr' => $abbr, 'state' => $state);
-        }
-        return $return;
-    }
+		foreach ($arr as $idx             => $title) {
+			$return[] = (object) array('idx' => $idx, 'title' => $title);
+		}
+	}
+
+	public function getUSStates() {
+		$arr = array(
+			'AL' => 'Alabama',
+			'AK' => 'Alaska',
+			'AS' => 'American',
+			'AZ' => 'Arizona',
+			'AR' => 'Arkansas',
+			'CA' => 'California',
+			'CO' => 'Colorado',
+			'CT' => 'Connecticut',
+			'DE' => 'Delaware',
+			'DC' => 'Dist. of Columbia',
+			'FL' => 'Florida',
+			'GA' => 'Georgia',
+			'GU' => 'Guam',
+			'HI' => 'Hawaii',
+			'ID' => 'Idaho',
+			'IL' => 'Illinois',
+			'IN' => 'Indiana',
+			'IA' => 'Iowa',
+			'KS' => 'Kansas',
+			'KY' => 'Kentucky',
+			'LA' => 'Louisiana',
+			'ME' => 'Maine',
+			'MD' => 'Maryland',
+			'MH' => 'Marshall',
+			'MA' => 'Massachusetts',
+			'MI' => 'Michigan',
+			'FM' => 'Micronesia',
+			'MN' => 'Minnesota',
+			'MS' => 'Mississippi',
+			'MO' => 'Missouri',
+			'MT' => 'Montana',
+			'NE' => 'Nebraska',
+			'NV' => 'Nevada',
+			'NH' => 'New Hampshire',
+			'NJ' => 'New Jersey',
+			'NM' => 'New Mexico',
+			'NY' => 'New York',
+			'NC' => 'North Carolina',
+			'ND' => 'North Dakota',
+			'MP' => 'Northern',
+			'OH' => 'Ohio',
+			'OK' => 'Oklahoma',
+			'OR' => 'Oregon',
+			'PW' => 'Palau',
+			'PA' => 'Pennsylvania',
+			'PR' => 'Puerto Rico',
+			'RI' => 'Rhode Island',
+			'SC' => 'South Carolina',
+			'SD' => 'South Dakota',
+			'TN' => 'Tennessee',
+			'TX' => 'Texas',
+			'UT' => 'Utah',
+			'VT' => 'Vermont',
+			'VA' => 'Virginia',
+			'VI' => 'Virgin Islands',
+			'WA' => 'Washington',
+			'WV' => 'West Virginia',
+			'WI' => 'Wisconsin',
+			'WY' => 'Wyoming');
+
+		foreach ($arr as $abbr             => $state) {
+			$return[] = (object) array('abbr' => $abbr, 'state' => $state);
+		}
+		return $return;
+	}
 }
