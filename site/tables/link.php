faaa7adc8b41201086e8ff4d44b112c9e8947c74
Matthew Murphy <matthew.murphy2@phila.gov>
2015-11-23 10:18:32 -0500

    Added link model

---
+++ site/tables/link.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * $Id: link.php $
+ * $LastChangedDate: 2015-05-24 $
+ * $LastChangedBy: Matt Murphy $
+ * Campaign Finance Reports - Philadelphiavotes.com
+ * a component for Joomla! 1.5 CMS (http://www.joomla.org)
+ * Author Website: http://www.philadelphiavotes.com
+ * @copyright Copyright (C) 2015 City of Philadelphia
+ * @license GNU/GPL http://www.gnu.org/copyleft/gpl.html
+ * @package Philadelphia.Votes
+ */
+
+defined('_JEXEC') or die('Restricted access');
+
+/**
+ * @package Philadelphia.Votes
+ */
+
+class PvmachineinspectorsLink extends JTable
+{
+    public $id;
+    public $address_id;
+    public $right_id;
+    public $created;
+    public $updated;
+
+    public function __construct(&$_db)
+    {
+        parent::__construct('#__pv_link', 'id', $_db);
+    }
+}
