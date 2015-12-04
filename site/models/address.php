a14a7987a632f967765c902c75538f054a3fc6f6
Matthew Murphy <matthew.murphy2@phila.gov>
2015-12-04 10:02:07 -0500

    Added docblocks and speculative params

--- site/models/address.php
+++ site/models/address.php
@@ -1,13 +1,13 @@
-<?php 
+<?php
 /**
  * $Id: site/models/address.php $
  * $LastChangedBy: Matt Murphy $
  * Election Officials - Philadelphiavotes.com
  * a component for Joomla! 1.5 CMS (http://www.joomla.org)
- * Author Website: http://www.philadelphiavotes.com
+ * Author Website: http://www.philadelphiavotes.com.
+ *
  * @copyright Copyright (C) 2015 City of Philadelphia
  * @license GNU/GPL http://www.gnu.org/copyleft/gpl.html
- * @package Philadelphia.Votes
  */
 
 // No direct access
@@ -16,24 +16,22 @@ defined('_JEXEC') or die;
 jimport('joomla.application.component.model');
 
 /**
- * User Component Remind Model
+ * User Component Remind Model.
  *
- * @package		Joomla
- * @subpackage	User
- * @since		1.5
+ * @since        1.5
  */
 class PvmachineinspectorsModelAddress extends JModel
 {
-	/**
-	 * Registry namespace prefix
-	 *
-	 * @var	string
-	 */
-	public $_namespace	= 'com_pvmachineinspectors.address.';
+    /**
+     * Registry namespace prefix.
+     *
+     * @var string
+     */
+    public $_namespace = 'com_pvmachineinspectors.address.';
 
     /**
      * Create a new applicant.
-     * 
+     *
      * @param  array
      *
      * @return bool
@@ -47,7 +45,7 @@ class PvmachineinspectorsModelAddress extends JModel
 
     /**
      * Read an address from address id.
-     * 
+     *
      * @param  int
      *
      * @return bool
@@ -59,7 +57,7 @@ class PvmachineinspectorsModelAddress extends JModel
 
     /**
      * Update an applicant.
-     * 
+     *
      * @param  array
      *
      * @return bool
@@ -71,10 +69,10 @@ class PvmachineinspectorsModelAddress extends JModel
 
     /**
      * Delete an applicant.
-     * 
+     *
      * @param  int
      *
-     * @return bool
+     * @return boolp
      */
     public function delete($id = null)
     {
--- site/models/applicant.php
+++ site/models/applicant.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * $Id: site/models/person.php $
  * $LastChangedBy: Matt Murphy $
