742a35063e2a79e67f6f3676cb9bfc8f8f937795
Matthew Murphy <matthew.e.murphy@phila.gov>
2015-12-05 22:36:56 -0500

    Cosmetic change

--- site/controller.php
+++ site/controller.php
@@ -21,179 +21,171 @@ jimport('joomla.application.component.controller');
  *
  * @since 1.5
  */
-class PvmachineinspectorsController extends JController
-{
-    /**
-     * Display signup form.
-     *
-     * @since   1.5
-     */
-    public function display()
-    {
-        JRequest::setVar('view', 'register');
-        parent::display();
-    }
+class PvmachineinspectorsController extends JController {
+	/**
+	 * Display signup form.
+	 *
+	 * @since   1.5
+	 */
+	public function display() {
+		JRequest::setVar('view', 'register');
+		parent::display();
+	}
 
-    /**
-     * Display signup acknowledgement.
-     *
-     * @since   1.5
-     */
-    public function thanks()
-    {
-        JRequest::setVar('view', 'thanks');
+	/**
+	 * Display signup acknowledgement.
+	 *
+	 * @since   1.5
+	 */
+	public function thanks() {
+		JRequest::setVar('view', 'thanks');
+		parent::display();
+	}
 
-        parent::display();
-    }
+	/**
+	 * Save registration and notify users and admins if required.
+	 */
+	public function register_save() {
+		$db = &JFactory::getDBO();
+		d('processing the save', $_POST);
 
-    /**
-     * Save registration and notify users and admins if required.
-     */
-    public function register_save()
-    {
-        $db = &JFactory::getDBO();
-        d('processing the save', $_POST);
+		// call to validate save, and ditch out to form on failure
+		if (!$this->validate_save()) {
+			// load the form and a message
+			$message = 'Form invalidated, sucka!';
+			// load the form again
+			return $this->display();
+		}
 
-        // call to validate save, and ditch out to form on failure
-        if (!$this->validate_save()) {
-            // load the form and a message
-            $message = 'Form invalidated, sucka!';
-            // load the form again
-            return $this->display();
-        }
+		if (!$this->save()) {
+			$message = 'Could not save. -- replace with a JError call';
+			return $this->display();
+		}
 
-        if (!$this->save()) {
-            $message = 'Could not save. -- replace with a JError call';
-            return $this->display();
-        }
+		// hey, we have good data!  let's set a message for the redirect
+		$message = "Thank you for registering to be a Machine Inspector.";
 
-        // hey, we have good data!  let's set a message for the redirect
-        $message = "Thank you for registering to be a Machine Inspector.";
+		//
+		$email = JRequest::getVar('email', null, 'post', 'string');
+		if ($email) {
+			$message .= "<br>...And thank you for providing an email address.  <br>At your convenience please check your email for a confirmation email and click the link within to <b>verify</b> your email.";
+		}
+		dd('stopping before we redirect');
+		$this->setRedirect('index.php', $message);
+	}
 
-        //
-        $email = JRequest::getVar('email', null, 'post', 'string');
-        if ($email) {
-            $message .= "<br>...And thank you for providing an email address.  <br>At your convenience please check your email for a confirmation email and click the link within to <b>verify</b> your email.";
-        }
-        dd('stopping before we redirect');
-        $this->setRedirect('index.php', $message);
-    }
+	/**
+	 * Validation tests for length only on fname, lname, address1, city, province, postcode, email
+	 *
+	 */
+	public function validate_save() {
+		return (JRequest::getVar('fname', null, 'post', 'string') &&
+			JRequest::getVar('lname', null, 'post', 'string') &&
+			JRequest::getVar('address1', null, 'post', 'string') &&
+			JRequest::getVar('city', null, 'post', 'string') &&
+			JRequest::getVar('province', null, 'post', 'string') &&
+			JRequest::getVar('postcode', null, 'post', 'string') &&
+			JRequest::getVar('email', null, 'post', 'string')
+		);
+	}
 
-    /**
-     * Validation tests for length only on fname, lname, address1, city, province, postcode, email
-     *
-     */
-    public function validate_save()
-    {
-        return (JRequest::getVar('fname', null, 'post', 'string') &&
-            JRequest::getVar('lname', null, 'post', 'string') &&
-            JRequest::getVar('address1', null, 'post', 'string') &&
-            JRequest::getVar('city', null, 'post', 'string') &&
-            JRequest::getVar('province', null, 'post', 'string') &&
-            JRequest::getVar('postcode', null, 'post', 'string') &&
-            JRequest::getVar('email', null, 'post', 'string')
-        );
-    }
+	/**
+	 * Save the form data in the various proper locations
+	 */
+	public function save() {
+		//
+		$ia = $this->getModel('applicant');
+		$a  = $this->getModel('address');
+		$l  = $this->getModel('link');
+		d(JPATH_COMPONENT.DS.'tables');
+		// create applicant record and get applicant id
+		$ia->create(
+			array(
+				'prefix' => JRequest::getVar('prefix', null, 'post', 'string'),
+				'fname'  => JRequest::getVar('fname', null, 'post', 'string'),
+				'mname'  => JRequest::getVar('mname', null, 'post', 'string'),
+				'lname'  => JRequest::getVar('lname', null, 'post', 'string'),
+				'suffix' => JRequest::getVar('suffix', null, 'post', 'string'),
+			)
+		);
+		/*        // save applicant links
 
-    /**
-     * Save the form data in the various proper locations
-     */
-    public function save()
-    {
-        //
-        $ia = $this->getModel('applicant');
-        $a = $this->getModel('address');
-        $l = $this->getModel('link');
-        d(JPATH_COMPONENT.DS.'tables');
-        // create applicant record and get applicant id
-        $ia->create(
-            array(
-                'prefix'=>JRequest::getVar('prefix', null, 'post', 'string'),
-                'fname' =>JRequest::getVar('fname', null, 'post', 'string'),
-                'mname' =>JRequest::getVar('mname', null, 'post', 'string'),
-                'lname' =>JRequest::getVar('lname', null, 'post', 'string'),
-                'suffix'=>JRequest::getVar('suffix', null, 'post', 'string'),
-            )
-        );
-/*        // save applicant links
+		//
+		// save the address
+		$a->create(
+		array(
+		'address1'=>JRequest::getVar('address1', null, 'post', 'string'),
+		'address2'=>JRequest::getVar('address2', null, 'post', 'string'),
+		'city'    =>JRequest::getVar('city', null, 'post', 'string'),
+		'province'=>JRequest::getVar('province', null, 'post', 'string'),
+		'postcode'=>JRequest::getVar('postcode', null, 'post', 'string'),
+		)
+		);
+		// bind the address to the applicant (person)
 
-        // 
-        // save the address
-        $a->create(
-            array(
-                'address1'=>JRequest::getVar('address1', null, 'post', 'string'),
-                'address2'=>JRequest::getVar('address2', null, 'post', 'string'),
-                'city'    =>JRequest::getVar('city', null, 'post', 'string'),
-                'province'=>JRequest::getVar('province', null, 'post', 'string'),
-                'postcode'=>JRequest::getVar('postcode', null, 'post', 'string'),
-            )
-        );
-        // bind the address to the applicant (person)
+		$l->create(
+		array(
+		'email'=>JRequest::getVar('email', null, 'post', 'string')
+		)
+		);
 
-        $l->create(
-            array(
-                'email'=>JRequest::getVar('email', null, 'post', 'string')
-            )
-        );
+		d($ia, $a, $l);*/
+		return true;
+	}
 
-        d($ia, $a, $l);*/
-        return true;
-    }
+	public function _sendMail(&$user, $password) {
+		global $mainframe;
 
-    public function _sendMail(&$user, $password)
-    {
-        global $mainframe;
+		$db = &JFactory::getDBO();
 
-        $db = &JFactory::getDBO();
+		$name     = $user->get('name');
+		$email    = $user->get('email');
+		$username = $user->get('username');
 
-        $name     = $user->get('name');
-        $email    = $user->get('email');
-        $username = $user->get('username');
+		$usersConfig    = &JComponentHelper::getParams('com_users');
+		$sitename       = $mainframe->getCfg('sitename');
+		$useractivation = $usersConfig->get('useractivation');
+		$mailfrom       = $mainframe->getCfg('mailfrom');
+		$fromname       = $mainframe->getCfg('fromname');
+		$siteURL        = JURI::base();
 
-        $usersConfig    = &JComponentHelper::getParams('com_users');
-        $sitename       = $mainframe->getCfg('sitename');
-        $useractivation = $usersConfig->get('useractivation');
-        $mailfrom       = $mainframe->getCfg('mailfrom');
-        $fromname       = $mainframe->getCfg('fromname');
-        $siteURL        = JURI::base();
+		$subject = sprintf(JText::_('Account details for'), $name, $sitename);
+		$subject = html_entity_decode($subject, ENT_QUOTES);
 
-        $subject = sprintf(JText::_('Account details for'), $name, $sitename);
-        $subject = html_entity_decode($subject, ENT_QUOTES);
+		if ($useractivation == 1) {
+			$message = sprintf(JText::_('SEND_MSG_ACTIVATE'), $name, $sitename, $siteURL.'index.php?option=com_user&task=activate&activation='.$user->get('activation'), $siteURL, $username, $password);
+		} else {
+			$message = sprintf(JText::_('SEND_MSG'), $name, $sitename, $siteURL);
+		}
 
-        if ($useractivation == 1) {
-            $message = sprintf(JText::_('SEND_MSG_ACTIVATE'), $name, $sitename, $siteURL.'index.php?option=com_user&task=activate&activation='.$user->get('activation'), $siteURL, $username, $password);
-        } else {
-            $message = sprintf(JText::_('SEND_MSG'), $name, $sitename, $siteURL);
-        }
+		$message = html_entity_decode($message, ENT_QUOTES);
 
-        $message = html_entity_decode($message, ENT_QUOTES);
+		//get all super administrator
+		$query = 'SELECT name, email, sendEmail'.
+		' FROM #__users'.
+		' WHERE LOWER( usertype ) = "super administrator"';
+		$db->setQuery($query);
+		$rows = $db->loadObjectList();
 
-        //get all super administrator
-        $query = 'SELECT name, email, sendEmail'.
-        ' FROM #__users'.
-        ' WHERE LOWER( usertype ) = "super administrator"';
-        $db->setQuery($query);
-        $rows = $db->loadObjectList();
+		// Send email to user
+		if (!$mailfrom || !$fromname) {
+			$fromname = $rows[0]->name;
+			$mailfrom = $rows[0]->email;
+		}
 
-        // Send email to user
-        if (!$mailfrom || !$fromname) {
-            $fromname = $rows[0]->name;
-            $mailfrom = $rows[0]->email;
-        }
+		JUtility::sendMail($mailfrom, $fromname, $email, $subject, $message);
 
-        JUtility::sendMail($mailfrom, $fromname, $email, $subject, $message);
+		// Send notification to all administrators
+		$subject2 = sprintf(JText::_('Account details for'), $name, $sitename);
+		$subject2 = html_entity_decode($subject2, ENT_QUOTES);
 
-        // Send notification to all administrators
-        $subject2 = sprintf(JText::_('Account details for'), $name, $sitename);
-        $subject2 = html_entity_decode($subject2, ENT_QUOTES);
-
-        // get superadministrators id
-        foreach ($rows as $row) {
-            if ($row->sendEmail) {
-                $message2 = sprintf(JText::_('SEND_MSG_ADMIN'), $row->name, $sitename, $name, $email, $username);
-                $message2 = html_entity_decode($message2, ENT_QUOTES);
-                JUtility::sendMail($mailfrom, $fromname, $row->email, $subject2, $message2);
-            }
-        }
-    }
+		// get superadministrators id
+		foreach ($rows as $row) {
+			if ($row->sendEmail) {
+				$message2 = sprintf(JText::_('SEND_MSG_ADMIN'), $row->name, $sitename, $name, $email, $username);
+				$message2 = html_entity_decode($message2, ENT_QUOTES);
+				JUtility::sendMail($mailfrom, $fromname, $row->email, $subject2, $message2);
+			}
+		}
+	}
 }
