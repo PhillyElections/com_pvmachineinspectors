c7275405d9402758eb4099a746375794f0713eaa
Matthew Murphy <matthew.murphy2@phila.gov>
2015-12-07 11:28:02 -0500

    still trying to view a table -- chosing a simpler one to call

--- site/models/applicant.php
+++ site/models/applicant.php
@@ -34,9 +34,9 @@ class PvmachineinspectorsModelApplicant extends JModel {
      */
     public function create($data = array()) {
         d(JPATH_COMPONENT_ADMINISTRATOR . DS . 'tables');
-        SELF::addIncludePath(JPATH_COMPONENT_ADMINISTRATOR . DS . 'tables');
+        parent::addIncludePath(JPATH_COMPONENT_ADMINISTRATOR . DS . 'tables');
         //$iat = $this->getTable('inspector_applicant');
-        $p = $this->getTable('person');
+        $p = parent::getTable('person');
         d($data, $this, $iat, $p);
         //save pv_person data and return a person_id
         // applicant loads ia and person
