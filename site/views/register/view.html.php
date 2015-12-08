013636740797fe86bd5ffbab728052cba25f73a0
Matthew Murphy <matthew.murphy2@phila.gov>
2015-12-08 10:18:07 -0500

    turned combo data into a library class

--- site/views/register/view.html.php
+++ site/views/register/view.html.php
@@ -20,18 +20,16 @@ jimport('joomla.application.component.view');
  *
  * @since 1.0
  */
-class PvmachineinspectorsViewRegister extends JView
-{
-    public function display()
-    {
+class PvmachineinspectorsViewRegister extends JView {
+    public function display() {
         global $mainframe;
-        $pathway  = &$mainframe->getPathway();
+        $pathway = &$mainframe->getPathway();
         $document = &JFactory::getDocument();
-        $params   = &$mainframe->getParams();
+        $params = &$mainframe->getParams();
 
         // Page Title
         $menus = &JSite::getMenu();
-        $menu  = $menus->getActive();
+        $menu = $menus->getActive();
 
         // fallback title
         $title = 'Machine Inspector Application';
@@ -66,105 +64,5 @@ class PvmachineinspectorsViewRegister extends JView
         JHTML::_('behavior.formvalidation');
 
         parent::display();
-    }
-
-    public function setComboData($arr)
-    {
-        foreach ($arr as $idx => $value) {
-            $return[] = (object) array('idx' => $idx, 'value' => $value);
-        }
-        return $return;
-    }
-
-    public function getPrefixes()
-    {
-        $arr = array(
-            '1' => '',
-            '2' => 'Mr.',
-            '3' => 'Mrs.',
-            '4' => 'Ms.',
-            '5' => 'Dr.',
-        );
-
-        return $this->setComboData($arr);
-    }
-
-    public function getSuffixes()
-    {
-        $arr = array(
-            '1' => '',
-            '2' => 'Jr.',
-            '3' => 'Sr.',
-            '4' => 'II',
-            '5' => 'III',
-        );
-
-        return $this->setComboData($arr);
-    }
-
-    public function getUSStates()
-    {
-        $arr = array(
-            'AL' => 'Alabama',
-            'AK' => 'Alaska',
-            'AS' => 'American',
-            'AZ' => 'Arizona',
-            'AR' => 'Arkansas',
-            'CA' => 'California',
-            'CO' => 'Colorado',
-            'CT' => 'Connecticut',
-            'DE' => 'Delaware',
-            'DC' => 'Dist. of Columbia',
-            'FL' => 'Florida',
-            'GA' => 'Georgia',
-            'GU' => 'Guam',
-            'HI' => 'Hawaii',
-            'ID' => 'Idaho',
-            'IL' => 'Illinois',
-            'IN' => 'Indiana',
-            'IA' => 'Iowa',
-            'KS' => 'Kansas',
-            'KY' => 'Kentucky',
-            'LA' => 'Louisiana',
-            'ME' => 'Maine',
-            'MD' => 'Maryland',
-            'MH' => 'Marshall',
-            'MA' => 'Massachusetts',
-            'MI' => 'Michigan',
-            'FM' => 'Micronesia',
-            'MN' => 'Minnesota',
-            'MS' => 'Mississippi',
-            'MO' => 'Missouri',
-            'MT' => 'Montana',
-            'NE' => 'Nebraska',
-            'NV' => 'Nevada',
-            'NH' => 'New Hampshire',
-            'NJ' => 'New Jersey',
-            'NM' => 'New Mexico',
-            'NY' => 'New York',
-            'NC' => 'North Carolina',
-            'ND' => 'North Dakota',
-            'MP' => 'Northern',
-            'OH' => 'Ohio',
-            'OK' => 'Oklahoma',
-            'OR' => 'Oregon',
-            'PW' => 'Palau',
-            'PA' => 'Pennsylvania',
-            'PR' => 'Puerto Rico',
-            'RI' => 'Rhode Island',
-            'SC' => 'South Carolina',
-            'SD' => 'South Dakota',
-            'TN' => 'Tennessee',
-            'TX' => 'Texas',
-            'UT' => 'Utah',
-            'VT' => 'Vermont',
-            'VA' => 'Virginia',
-            'VI' => 'Virgin Islands',
-            'WA' => 'Washington',
-            'WV' => 'West Virginia',
-            'WI' => 'Wisconsin',
-            'WY' => 'Wyoming');
-
-        return $this->setComboData($arr);
     }
 }
