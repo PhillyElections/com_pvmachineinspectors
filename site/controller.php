254cdffc4cb6119076efcb638950c1592cc64258
Matthew Murphy <matthew.murphy2@phila.gov>
2015-12-16 11:31:24 -0500

    reworking the new server-side validation

--- site/controller.php
+++ site/controller.php
@@ -150,6 +150,7 @@ class PvmachineinspectorsController extends JController {
             preg_replace('/^1|\D/', "", JRequest::getVar('phone', null, 'post')),
             preg_replace("/[^0-9,.]/", "", JRequest::getVar('phone', null, 'post')),
             filter_var(JRequest::getVar('postcode', null, 'post'), FILTER_VALIDATE_INT),
+            filter_var(JRequest::getVar('postcode', null, 'post'), FILTER_SANITIZE_INT),
             filter_var(JRequest::getVar('postcode', null, 'post'), FILTER_SANITIZE_INT) === JRequest::getVar('postcode', null, 'post')
         );
         return !$invalid;
