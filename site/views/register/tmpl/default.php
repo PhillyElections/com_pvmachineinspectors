3058aa65c693b2dd1aa74097133ec8041776d671
Matthew Murphy <matthew.murphy2@phila.gov>
2015-12-18 09:44:41 -0500

    We're able to bind an array, let's display it.

--- site/views/register/tmpl/default.php
+++ site/views/register/tmpl/default.php
@@ -10,7 +10,7 @@ if (count(JRequest::getVar('msg', null, 'post'))) {
     echo "</ul>\n</dd>\n</dl>\n";
 
     foreach (JRequest::getVar('msg', null, 'post') as $msg) {
-        JError::raiseNotice(1, $msg);
+        JError::raiseWarning(1, $msg);
     }
 
 }
