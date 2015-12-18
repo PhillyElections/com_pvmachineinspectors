ac5e7b7ad3b9741e1a9f3dd0f60333432b0f4372
Matthew Murphy <matthew.murphy2@phila.gov>
2015-12-18 12:41:52 -0500

    let's get our own parent model to abstract the shared stuff out to

---
+++ site/models/pvmodel.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * $Id: site/models/address.php $
+ * $LastChangedBy: Matt Murphy $
+ * Election Officials - Philadelphiavotes.com
+ * a component for Joomla! 1.5 CMS (http://www.joomla.org)
+ * Author Website: http://www.philadelphiavotes.com.
+ *
+ * @copyright Copyright (C) 2015 City of Philadelphia
+ * @license GNU/GPL http://www.gnu.org/copyleft/gpl.html
+ */
+
+// No direct access
+defined('_JEXEC') or die;
+
+jimport('joomla.application.component.model');
+
+/**
+ * User Component Remind Model.
+ *
+ * @since        1.5
+ */
+class PVModel extends JModel {
+
+    public function getXrefTableId($XrefTo) {
+        $x2 = $this->getTable('Table', $XrefTo);
+        $t = $this->getTable('Table', 'PVTable');
+
+        $tableName = JString::str_ireplace('#__', $x2->_db->getPrefix(), $x2->getTableName());
+
+        $t->loadFromKeyValuePairs(array('name' => $tableName));
+        return $t->get('id');
+    }
+}
