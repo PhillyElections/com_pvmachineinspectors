342e88e92681bf9dce26856a6740fea7de7a1fb6
Matthew Murphy <matthew.murphy2@phila.gov>
2015-12-21 10:00:10 -0500

    collapsing to single table

--- admin/sql/uninstall.sql
+++ admin/sql/uninstall.sql
@@ -1,9 +1,2 @@
 DROP TABLE 
-`#__pv_addresses`, 
-`#__pv_address_xrefs`, 
-`#__pv_inspector_applicants`,
-`#__pv_links`, 
-`#__pv_link_types`, 
-`#__pv_link_xrefs`, 
-`#__pv_persons`, 
-`#__pv_tables`;
+`#__pv_inspector_applicants`,;
