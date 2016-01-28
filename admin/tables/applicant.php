29dbd8b3743ba83eeca5e085cae713cfe87760ff
Matthew Murphy <matthew.murphy2@phila.gov>
2016-01-28 16:14:09 -0500

    let's override check() in the table

--- admin/tables/applicant.php
+++ admin/tables/applicant.php
@@ -43,4 +43,68 @@ class TableApplicant extends JTable
     {
         parent::__construct('#__pv_inspector_applicants', 'id', $_db);
     }
+
+    /**
+     * override parent::check
+     *
+     * @return boolean
+     */
+    public function check()
+    {
+        // we need a first_name
+        if (trim($this->first_name) === '') {
+            $this->setError('First name is required.');
+            return false;
+        }
+
+        // we need a last_name
+        if (trim($this->last_name) === '') {
+            $this->setError('Last name is required.');
+            return false;
+        }
+
+        // we need an address1
+        if (trim($this->address1) === '') {
+            $this->setError('A street address is required.');
+            return false;
+        }
+
+        // we need a city
+        if (trim($this->city) === '') {
+            $this->setError('A city is required.');
+            return false;
+        }
+
+        // we need a 2-digit region
+        if (!(JString::strlen(trim($this->region)) === 2)) {
+            $this->setError('A state is required.');
+            return false;
+        }
+
+        // we need a 5 numeric digits starting from the left in out postcode
+        if (!(filter_var(trim($this->postcode), FILTER_SANITIZE_NUMBER_INT) === trim($this->postcode))) {
+            $this->setError('A valid zipcode is required.');
+            return false;
+        }
+
+        // if we have an email, we need a valid email
+        if (trim($this->email) && !filter_var(trim($this->email), FILTER_VALIDATE_EMAIL)) {
+            $this->setError(trim($this->email) . ' is not a valid email.');
+        }
+
+        // if we have a phone we need a valid phone
+        if (trim($this->phone)) {
+            // reject phone numbers with letters in them
+            if (JString::strlen(trim($this->phone))) {
+                $this->setError('Please supply a phone using numbers only.');
+                return false;
+            }
+            // Phone numbers may be given with the leading '1' or not
+            if (JString::strlen(preg_replace('/^1|\D/', "", trim($this->phone))) !== 10) {
+                $this->setError('Your phone number doesn\'t seem to be the normal length (10 digits). Please reenter.');
+                return false;
+            }
+        }
+        return true;
+    }
 }
