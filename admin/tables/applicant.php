80761518847c2bf9e8910016ea53197415eb196e
Matthew Murphy <matthew.murphy2@phila.gov>
2016-02-03 10:26:19 -0500

    ok, ok, technically, i should use JString::trim()

--- admin/tables/applicant.php
+++ admin/tables/applicant.php
@@ -53,48 +53,48 @@ class TableApplicant extends JTable
     public function check()
     {
         // we need a first_name
-        if (trim($this->first_name) === '') {
+        if (!JString::trim($this->first_name)) {
             $this->setError('First name is required.');
         }
 
         // we need a last_name
-        if (trim($this->last_name) === '') {
+        if (!JString::trim($this->last_name)) {
             $this->setError('Last name is required.');
         }
 
         // we need an address1
-        if (trim($this->address1) === '') {
+        if (!JString::trim($this->address1)) {
             $this->setError('A street address is required.');
         }
 
         // we need a city
-        if (trim($this->city) === '') {
+        if (!JString::trim($this->city)) {
             $this->setError('A city is required.');
         }
 
         // we need a 2-digit region
-        if (!(JString::strlen(trim($this->region)) === 2)) {
+        if (JString::strlen(trim($this->region)) !== 2) {
             $this->setError('A state is required.');
         }
 
         // we need a 5 numeric digits starting from the left in out postcode
-        if (!(filter_var(trim($this->postcode), FILTER_SANITIZE_NUMBER_INT) === trim($this->postcode))) {
+        if (!(filter_var(JString::trim($this->postcode), FILTER_SANITIZE_NUMBER_INT) === JString::trim($this->postcode))) {
             $this->setError('A valid zipcode is required.');
         }
 
         // if we have an email, we need a valid email
-        if (trim($this->email) && !filter_var(trim($this->email), FILTER_VALIDATE_EMAIL)) {
-            $this->setError(trim($this->email) . ' is not a valid email.');
+        if (JString::trim($this->email) && !filter_var(JString::trim($this->email), FILTER_VALIDATE_EMAIL)) {
+            $this->setError(JString::trim($this->email) . ' is not a valid email.');
         }
 
         // if we have a phone we need a valid phone
         if (trim($this->phone)) {
             // reject phone numbers with letters in them
-            if (!is_numeric(trim($this->phone))) {
+            if (!is_numeric(JString::trim($this->phone))) {
                 $this->setError('Please supply a phone using numbers only.');
             }
             // Phone numbers may be given with the leading '1' or not
-            if (JString::strlen(preg_replace('/^1|\D/', "", trim($this->phone))) !== 10) {
+            if (JString::strlen(preg_replace('/^1|\D/', "", JString::trim($this->phone))) !== 10) {
                 $this->setError('Your phone number doesn\'t seem to be the normal length (10 digits). Please reenter.');
             }
         }
