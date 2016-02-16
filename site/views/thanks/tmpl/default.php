94324002f8490e0c6f2153abb65e992dd5073c38
Matthew Murphy <matthew.e.murphy@phila.gov>
2016-02-16 12:02:00 -0500

    lets test existing translation strings and our new integrated translation.

--- site/views/thanks/tmpl/default.php
+++ site/views/thanks/tmpl/default.php
@@ -5,7 +5,7 @@ defined('_JEXEC') or die('Restricted access');?>
 <?php
 
 if (isset($this->message)) {
-    $this->display('message');
+	$this->display('message');
 }
 ?>
-<p>Thank you for registering to be a Machine Inspector in the next election!</p>
+<p><?=JText::_("THANK YOU")?></p>
