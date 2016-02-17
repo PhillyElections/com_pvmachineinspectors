b6472f357877b379e9ac9d4537e941df003c6c3a
Matthew Murphy <matthew.e.murphy@phila.gov>
2016-02-17 16:34:48 -0500

    wiring applicant view for language files

--- admin/views/applicant/tmpl/default.php
+++ admin/views/applicant/tmpl/default.php
@@ -15,28 +15,28 @@ $applicant = $this->applicant;
 <table cellpadding="0" cellspacing="0" border="0" width="100%" class="contentpane">
 <tr>
     <td width="30%" height="40">
-        <label id="namemsg" for="first_name"><?=JText::_('Name');?>:</label>
+        <label id="namemsg" for="first_name"><?=JText::_('NAME');?>:</label>
     </td>
     <td>
 <?=JHTML::_('select.genericlist', PVCombo::gets('prefix'), 'prefix', 'class="inputbox required"', 'idx', 'value', PVCombo::keySearch('prefix', $appliant->prefix), true)?>
-        <input type="text" name="first_name" id="first_name" size="18%" value="<?=$applicant->first_name?>" class="inputbox required" maxlength="50" placeholder="(firstname is required)" />
+        <input type="text" name="first_name" id="first_name" size="18%" value="<?=$applicant->first_name?>" class="inputbox required" maxlength="50" placeholder="<?=JText::_('FNAME PLACEHOLDER');?>" />
         <input type="text" name="middle_name" id="middle_name" size="1%" value="<?=$applicant->middle_name?>" class="inputbox optional" maxlength="25" />
-        <input type="text" name="last_name" id="last_name" size="18%" value="<?=$applicant->last_name?>" class="inputbox required" maxlength="50" placeholder="(lastname is required)" />
+        <input type="text" name="last_name" id="last_name" size="18%" value="<?=$applicant->last_name?>" class="inputbox required" maxlength="50" placeholder="<?=JText::_('LNAME PLACEHOLDER');?>" />
 <?=JHTML::_('select.genericlist', PVCombo::gets('suffix'), 'suffix', 'class="inputbox required"', 'idx', 'value', PVCombo::keySearch('suffix', $appliant->suffix), true)?>
     </td>
 
 </tr>
 <tr>
     <td height="40">
-        <label id="address1msg" for="address1"><?=JText::_('Street Address');?>:</label>
+        <label id="address1msg" for="address1"><?=JText::_('STREET NAME');?>:</label>
     </td>
     <td>
-        <input type="text" id="address1" name="address1" size="60%" value="<?=$applicant->address1?>" class="inputbox required" maxlength="60" placeholder="(street address is required)" />
+        <input type="text" id="address1" name="address1" size="60%" value="<?=$applicant->address1?>" class="inputbox required" maxlength="60" placeholder="<?=JText::_('STREET ADDRESS PLACEHOLDER');?>" />
     </td>
 </tr>
 <tr>
     <td height="40">
-        <label id="address2msg" for="address2"><?=JText::_('Apt/Unit/Suite');?>:</label>
+        <label id="address2msg" for="address2"><?=JText::_('APT_UNIT_SUITE');?>:</label>
     </td>
     <td>
         <input type="text" id="address2" name="address2" size="60%" value="<?=$applicant->address2?>" class="inputbox optional" maxlength="60" />
@@ -44,15 +44,15 @@ $applicant = $this->applicant;
 </tr>
 <tr>
     <td height="40">
-        <label id="citymsg" for="city"><?=JText::_('City');?>:</label>
+        <label id="citymsg" for="city"><?=JText::_('CITY');?>:</label>
     </td>
     <td>
-        <input type="text" id="city" name="city" size="60%" value="<?=($applicant->city?$applicant->city:'Philadelphia')?>" class="inputbox required" maxlength="60" placeholder="(city is required)" />
+        <input type="text" id="city" name="city" size="60%" value="<?=($applicant->city?$applicant->city:'Philadelphia')?>" class="inputbox required" maxlength="60" placeholder="<?=JText::_('CITY REQUIRED');?>" />
     </td>
 </tr>
 <tr>
     <td height="40">
-        <label id="regionmsg" for="region"><?=JText::_('State');?>:</label>
+        <label id="regionmsg" for="region"><?=JText::_('STATE');?>:</label>
     </td>
     <td>
 <?=JHTML::_('select.genericlist', PVCombo::gets('state'), 'region', 'class="inputbox required"', 'idx', 'value', ($applicant->region?PVCombo::keySearch('state', $appliant->region):'PA'), true)?>
@@ -60,32 +60,32 @@ $applicant = $this->applicant;
 </tr>
 <tr>
     <td height="40">
-        <label id="postcodemsg" for="postcode"><?=JText::_('Zipcode');?>:</label>
+        <label id="postcodemsg" for="postcode"><?=JText::_('ZIPCODE');?>:</label>
     </td>
     <td>
-        <input type="text" id="postcode" name="postcode" size="60%" value="<?=$applicant->postcode?>" class="inputbox required" maxlength="60" placeholder="(zip is required)" />
+        <input type="text" id="postcode" name="postcode" size="60%" value="<?=$applicant->postcode?>" class="inputbox required" maxlength="60" placeholder=<?=JText::_('ZIPCODE PLACEHOLDER');?> />
     </td>
 </tr>
 <tr>
     <td height="40">
-        <label id="phonemsg" for="phone"><?=JText::_('Phone');?>:</label>
+        <label id="phonemsg" for="phone"><?=JText::_('PHONE');?>:</label>
     </td>
     <td>
-        <input type="text" id="phone" name="phone" size="60%" value="<?=$applicant->phone?>" class="inputbox" maxlength="100" placeholder="(either email or phone required)" />
+        <input type="text" id="phone" name="phone" size="60%" value="<?=$applicant->phone?>" class="inputbox" maxlength="100" placeholder="<?=JText::_('PHONE PLACEHOLDER');?>" />
     </td>
 </tr>
 <tr>
     <td height="40">
-        <label id="emailmsg" for="email"><?=JText::_('Email');?>:</label>
+        <label id="emailmsg" for="email"><?=JText::_('EMAIL');?>:</label>
     </td>
     <td>
-        <input type="text" id="email" name="email" size="60%" value="<?=$applicant->email?>" class="inputbox" maxlength="100" placeholder="(either email or phone required)" />
+        <input type="text" id="email" name="email" size="60%" value="<?=$applicant->email?>" class="inputbox" maxlength="100"  />
     </td>
 </tr>
 <tr>
     <td height="40">&nbsp;</td>
     <td>
-        <button class="button validate" type="submit"><?=JText::_('Update');?></button>
+        <button class="button validate" type="submit"><?=JText::_('UPDATE');?></button>
         <input type="hidden" name="task" value="update" />
         <input type="hidden" name="controller" value="applicant" />
         <input type="hidden" name="id" value="<?=$applicant->id?>" />
