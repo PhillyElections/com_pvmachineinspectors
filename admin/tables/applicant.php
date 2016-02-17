908109f1cce511edbf4c4391bc883f01c76e7cdb
Matthew Murphy <matthew.e.murphy@phila.gov>
2016-02-17 15:36:29 -0500

    moving validation messages into language files

--- admin/tables/applicant.php
+++ admin/tables/applicant.php
@@ -50,47 +50,50 @@ class TableApplicant extends JTable {
 	public function check() {
 		// we need a first_name
 		if (!JString::trim($this->first_name)) {
-			$this->setError('First name is required.');
+			$this->setError(JText::_('VALIDATION FIRSTNAME REQUIRED'));
 		}
 
 		// we need a last_name
 		if (!JString::trim($this->last_name)) {
-			$this->setError('Last name is required.');
+			$this->setError(JText::_('VALIDATION LASTNAME REQUIRED'));
 		}
 
 		// we need an address1
 		if (!JString::trim($this->address1)) {
-			$this->setError('A street address is required.');
+			$this->setError(JText::_('VALIDATION STREET ADDRESS REQUIRED'));
 		}
 
 		// we need a city
 		if (!JString::trim($this->city)) {
-			$this->setError('A city is required.');
+			$this->setError(JText::_('VALIDATION CITY REQUIRED'));
 		}
 
 		// we need a 2-digit region
 		if (JString::strlen(trim($this->region)) !== 2) {
-			$this->setError('A state is required.');
+			$this->setError(JText::_('VALIDATION STATE REQUIRED'));
 		}
 
 		// we need a 5 numeric digits starting from the left in out postcode
 		if (!is_numeric(JString::trim($this->postcode))) {
-			$this->setError('A valid zipcode is required.');
+			$this->setError(JText::_('VALIDATION ZIPCODE REQUIRED'));
 		}
 
 		// if we have an email, we need a valid email
 		if (JString::trim($this->email) && !filter_var(JString::trim($this->email), FILTER_VALIDATE_EMAIL)) {
-			$this->setError(JString::trim($this->email).' is not a valid email.');
+			$this->setError(JString::trim($this->email).JText::_('VALIDATION EMAIL INVALID'));
 		}
 
-		// if we have a phone we need a valid phone
-		// reject phone numbers with letters in them
-		if (!is_numeric(JString::trim($this->phone))) {
-			$this->setError('Please supply a phone using numbers only.');
-		}
-		// Phone numbers may be given with the leading '1' or not
-		if (JString::strlen(preg_replace('/^1|\D/', "", JString::trim($this->phone))) !== 10) {
-			$this->setError('Your phone number doesn\'t seem to be the normal length (10 digits). Please reenter.');
+		if (JString::strlen(JString::trim($this->phone))) {
+			// reject phone numbers with letters in them
+			if (!is_numeric(JString::trim($this->phone))) {
+				$this->setError(JText::_('VALIDATION PHONE NUMERIC'));
+			}
+			// Phone numbers may be given with the leading '1' or not
+			if (JString::strlen(preg_replace('/^1|\D/', "", JString::trim($this->phone))) !== 10) {
+				$this->setError(JText::_('VALIDATION PHONE LENGTH'));
+			}
+		} else {
+			$this->setError(JText::_('VALIDATION PHONE EMPTY'));
 		}
 
 		if (count($this->getErrors())) {
