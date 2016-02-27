400d1fb8d248050d9c6ce173966b90651c181e09
Matthew Murphy <matthew.murphy2@phila.gov>
2016-02-27 09:37:51 -0500

    Let's work our way back to select/option for the state

--- site/views/register/tmpl/default.php
+++ site/views/register/tmpl/default.php
@@ -61,7 +61,7 @@ $document->addCustomTag('<script src="/components/com_pvmachineinspectors/assets
 <?=JText::_('REGION');?>:
 		</label>
 	</td>
-	<td><? /*=JHTML::_('select.genericlist', PVCombo::gets('state'), 'region', 'class="inputbox required"', 'idx', 'value', ($region ? $region : 'PA'), true)*/?><input type="text" id="state" name="state" size="60%" value="<?=($region ? $region : 'PA')?>" /></td>
+	<td><?=JHTML::_('select.genericlist', PVCombo::gets('state'), 'region', 'class="inputbox required"', 'idx', 'value', ($region ? $region : 'PA'), true)?><?//<input type="text" id="state" name="state" size="60%" value="<?=($region ? $region : 'PA')?>" /></td>
 </tr>
 <tr>
 	<td height="40">
