da6720d5f645217d9dbd82380d711ba7ab560fde
Matthew Murphy <matthew.murphy2@phila.gov>
2016-02-29 09:51:12 -0500

    formatting

--- site/views/register/view.html.php
+++ site/views/register/view.html.php
@@ -20,36 +20,38 @@ jimport('joomla.application.component.view');
  *
  * @since 1.0
  */
-class PvmachineinspectorsViewRegister extends JView {
-	public function display() {
-		global $mainframe;
-		$pathway  = &$mainframe->getPathway();
-		$document = &JFactory::getDocument();
-		$params   = &$mainframe->getParams();
+class PvmachineinspectorsViewRegister extends JView
+{
+    public function display()
+    {
+        global $mainframe;
+        $pathway = &$mainframe->getPathway();
+        $document = &JFactory::getDocument();
+        $params = &$mainframe->getParams();
 
-		// Page Title
-		$menus = &JSite::getMenu();
-		$menu  = $menus->getActive();
+        // Page Title
+        $menus = &JSite::getMenu();
+        $menu = $menus->getActive();
 
-		// fallback title
-		$title = JText::_('APPLY TO BE A MACHINE INSPECTOR');
+        // fallback title
+        $title = JText::_('APPLY TO BE A MACHINE INSPECTOR');
 
-		if (is_object($menu)) {
-			$menu_params = new JParameter($menu->params);
-			if (!$menu_params->get('page_title')) {
-				$params->set('page_title', $title);
-			}
-		} else {
-			$params->set('page_title', $title);
-		}
+        if (is_object($menu)) {
+            $menu_params = new JParameter($menu->params);
+            if (!$menu_params->get('page_title')) {
+                $params->set('page_title', $title);
+            }
+        } else {
+            $params->set('page_title', $title);
+        }
 
-		// assign
-		$document->setTitle($params->get('page_title'));
-		$this->assignRef('params', $params);
+        // assign
+        $document->setTitle($params->get('page_title'));
+        $this->assignRef('params', $params);
 
-		// Load the form validation behavior
-		JHTML::_('behavior.formvalidation');
+        // Load the form validation behavior
+        JHTML::_('behavior.formvalidation');
 
-		parent::display();
-	}
+        parent::display();
+    }
 }
