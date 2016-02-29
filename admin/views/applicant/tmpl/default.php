62b13af16e37e62dbd0cfd35ec4befc9b0f6703a
Matthew Murphy <matthew.murphy2@phila.gov>
2016-02-29 14:01:23 -0500

    Let's get those combos running...

--- admin/views/applicant/tmpl/default.php
+++ admin/views/applicant/tmpl/default.php
@@ -12,7 +12,7 @@ if (count(JRequest::getVar('msg', null, 'post'))) {
     }
 }
 $applicant = $this->applicant;
-d(PVCombo::keySearch('suffix', $applicant->suffix), PVCombo::get('state', $applicant->region));
+d(PVCombo::get('suffix', PVCombo::keySearch('suffix', $applicant->suffix)), PVCombo::get('state', $applicant->region));
 
 $document->addCustomTag('<script src="/components/com_pvmachineinspectors/assets/js/machineinspectors.js" async defer></script>');
 ?>
@@ -26,11 +26,11 @@ $document->addCustomTag('<script src="/components/com_pvmachineinspectors/assets
         </label>
     </td>
     <td>
-        <?=JHTML::_('select.genericlist', PVCombo::gets('prefix'), 'prefix', 'class="inputbox required"', 'idx', 'value', PVCombo::get('prefix', $applicant->prefix), true)?>
+        <?=JHTML::_('select.genericlist', PVCombo::gets('prefix'), 'prefix', 'class="inputbox required"', 'idx', 'value', PVCombo::get('prefix', PVCombo::keySearch('prefix', $applicant->prefix)), true)?>
         <input type="text" name="first_name" id="first_name" size="18%" value="<?=$applicant->first_name?>" class="inputbox required" maxlength="50" placeholder="(firstname is required)" />
         <input type="text" name="middle_name" id="middle_name" size="1%" value="<?=$applicant->middle_name?>" class="inputbox optional" maxlength="25" />
         <input type="text" name="last_name" id="last_name" size="18%" value="<?=$applicant->last_name?>" class="inputbox required" maxlength="50" placeholder="(lastname is required)" />
-        <?=JHTML::_('select.genericlist', PVCombo::gets('suffix'), 'suffix', 'class="inputbox required"', 'idx', 'value', PVCombo::get('suffix', $applicant->suffix), true)?>
+        <?=JHTML::_('select.genericlist', PVCombo::gets('suffix'), 'suffix', 'class="inputbox required"', 'idx', 'value', PVCombo::get('suffix', PVCombo::keySearch('suffix', $applicant->suffix)), true)?>
     </td>
 
 </tr>
