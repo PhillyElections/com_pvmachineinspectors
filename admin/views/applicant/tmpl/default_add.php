8585345387d38f6db1c3d5ed815e15e8fbfefc64
Matthew Murphy <matthew.murphy2@phila.gov>
2016-03-03 03:32:23 -0500

    adding the google lookup to the 'new' backend form

--- admin/views/applicant/tmpl/default_add.php
+++ admin/views/applicant/tmpl/default_add.php
@@ -15,6 +15,8 @@ foreach ($fields as $field) {
     $$field = JRequest::getVar($field, null, 'post');
 }
 
+$document->addCustomTag('<script src="/components/com_pvmachineinspectors/assets/js/machineinspectors.js" async defer></script>');
+
 ?>
 <form action="<?=JRoute::_('index.php?option=com_pvmachineinspectors');?>" method="post" id="adminForm" name="adminForm" class="form-validate">
 
