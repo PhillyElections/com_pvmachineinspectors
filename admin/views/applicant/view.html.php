9a5b67b87bfc5d3660f822f652d88a478c42f199
Matthew Murphy <matthew.e.murphy@phila.gov>
2016-03-03 14:43:13 -0500

    wiring up cancel

--- admin/views/applicant/view.html.php
+++ admin/views/applicant/view.html.php
@@ -19,35 +19,33 @@ jimport('joomla.application.component.view');
  * @package    Joomla.Tutorials
  * @subpackage Components
  */
-class PvmachineinspectorsViewApplicant extends JView
-{
-    /**
-     * display method of Applicant view
-     * @return void
-     **/
-    public function display($tpl = null)
-    {
+class PvmachineinspectorsViewApplicant extends JView {
+	/**
+	 * display method of Applicant view
+	 * @return void
+	 **/
+	public function display($tpl = null) {
 
-        $applicant = &$this->get('Data');
+		$applicant = &$this->get('Data');
 
-        $isNew = ($applicant->id < 1);
+		$isNew = ($applicant->id < 1);
 
-        $text = $isNew ? JText::_('New') : JText::_('Edit');
-        JToolBarHelper::title(JText::_('Applicant') . ': <small><small>[ ' . $text . ' ]</small></small>');
-        if ($isNew) {
-            JToolBarHelper::save('save', 'Register');
-            JToolBarHelper::cancel();
-            // We'll use a separate template for new applicants: default_add
-            $tpl = 'add';
-        } else {
-            // for existing items the button is renamed `close`
-            JToolBarHelper::save('save', 'Update');
-            JToolBarHelper::cancel('cancel', 'Close');
-        }
+		$text = $isNew?JText::_('New'):JText::_('Edit');
+		JToolBarHelper::title(JText::_('Applicant').': <small><small>[ '.$text.' ]</small></small>');
+		if ($isNew) {
+			JToolBarHelper::save('save', 'Register');
+			JToolBarHelper::cancel('cancel', 'Close');
+			// We'll use a separate template for new applicants: default_add
+			$tpl = 'add';
+		} else {
+			// for existing items the button is renamed `close`
+			JToolBarHelper::save('save', 'Update');
+			JToolBarHelper::cancel('cancel', 'Close');
+		}
 
-        $this->assignRef('applicant', $applicant);
-        $this->assignRef('isNew', $isNew);
+		$this->assignRef('applicant', $applicant);
+		$this->assignRef('isNew', $isNew);
 
-        parent::display($tpl);
-    }
+		parent::display($tpl);
+	}
 }
