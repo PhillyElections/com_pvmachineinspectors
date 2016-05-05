ca6a728e1fc7b4625d1fade2f551e52ae964c02a
matthew murphy <mattyhead@gmail.com>
2016-05-05 07:38:11 -0400

    Let's explicitly count (and report for now)

--- admin/tables/applicant.php
+++ admin/tables/applicant.php
@@ -48,70 +48,67 @@ class TableApplicant extends JTable
      */
     public function check()
     {
-        $error = array();
+        $error = 0;
         // we need a first_name
         if (!JString::trim($this->first_name)) {
-            $error[] = JText::_('VALIDATION FIRSTNAME REQUIRED');
-            //$this->setError(JText::_('VALIDATION FIRSTNAME REQUIRED'));
-
+            $this->setError(JText::_('VALIDATION FIRSTNAME REQUIRED'));
+            $error++;
         }
 
         // we need a last_name
         if (!JString::trim($this->last_name)) {
-            $error[] = JText::_('VALIDATION LASTNAME REQUIRED');
-            // $this->setError(JText::_('VALIDATION LASTNAME REQUIRED'));
+            $this->setError(JText::_('VALIDATION LASTNAME REQUIRED'));
+            $error++;
         }
 
         // we need an address1
         if (!JString::trim($this->address1)) {
-            $error[] = JText::_('VALIDATION STREET ADDRESS REQUIRED');
-            // $this->setError(JText::_('VALIDATION STREET ADDRESS REQUIRED'));
+            $this->setError(JText::_('VALIDATION STREET ADDRESS REQUIRED'));
+            $error++;
         }
 
         // we need a city
         if (!JString::trim($this->city)) {
-            $error[] = JText::_('VALIDATION CITY REQUIRED');
-            // $this->setError(JText::_('VALIDATION CITY REQUIRED'));
+            $this->setError(JText::_('VALIDATION CITY REQUIRED'));
+            $error++;
         }
 
         // we need a 2-digit region
         if (JString::strlen(trim($this->region)) !== 2) {
-            $error[] = JText::_('VALIDATION STATE REQUIRED');
-            // $this->setError(JText::_('VALIDATION STATE REQUIRED'));
+            $this->setError(JText::_('VALIDATION STATE REQUIRED'));
+            $error++;
         }
 
         // we need a 5 numeric digits starting from the left in out postcode
         if (!is_numeric(JString::trim($this->postcode))) {
-            $error[] = JText::_('VALIDATION ZIPCODE REQUIRED');
-            // $this->setError(JText::_('VALIDATION ZIPCODE REQUIRED'));
+            $this->setError(JText::_('VALIDATION ZIPCODE REQUIRED'));
+            $error++;
         }
 
         // if we have an email, we need a valid email
         if (JString::trim($this->email) && !filter_var(JString::trim($this->email), FILTER_VALIDATE_EMAIL)) {
-            $error[] = JString::trim($this->email) . JText::_('VALIDATION EMAIL INVALID');
-            // $this->setError(JString::trim($this->email) . JText::_('VALIDATION EMAIL INVALID'));
+            $this->setError(JString::trim($this->email) . JText::_('VALIDATION EMAIL INVALID'));
+            $error++;
         }
 
         if (JString::strlen(JString::trim($this->phone))) {
             // reject phone numbers with letters in them
             if (!is_numeric(JString::trim($this->phone))) {
-                $error[] = JText::_('VALIDATION PHONE NUMERIC');
-                // $this->setError(JText::_('VALIDATION PHONE NUMERIC'));
+                $this->setError(JText::_('VALIDATION PHONE NUMERIC'));
+                $error++;
             }
             // Phone numbers may be given with the leading '1' or not
             if (JString::strlen(preg_replace('/^1|\D/', "", JString::trim($this->phone))) !== 10) {
-                $error[] = JText::_('VALIDATION PHONE LENGTH');
-                // $this->setError(JText::_('VALIDATION PHONE LENGTH'));
+                $this->setError(JText::_('VALIDATION PHONE LENGTH'));
+                $error++;
             }
         } else {
-            $error[] = JText::_('VALIDATION PHONE EMPTY');
-            // $this->setError(JText::_('VALIDATION PHONE EMPTY'));
+            $this->setError(JText::_('VALIDATION PHONE EMPTY'));
+            $error++;
         }
 
-        //if (count($this->getErrors())) {
-        if (count($error)) {
+        if ($error) {
             d($error);
-            $this->setError(implode('<br>', $error));
             return false;
         }
         return true;
