2adfdf84d8ea781929d0bd2f63bc720f64024439
matthew murphy <mattyhead@gmail.com>
2016-05-07 17:04:50 -0400

    5 and 5

--- admin/views/applicants/tmpl/default.php
+++ admin/views/applicants/tmpl/default.php
@@ -18,9 +18,9 @@ $document->addStyleSheet('components/com_pvmachineinspectors/assets/css/filter.c
 if (count($this->items) or JRequest::getVar('ward')):
 ?>
                 <tr>
-                    <th colspan="4" data-filter="Filter by Wards">
+                    <th colspan="5" data-filter="Filter by Wards">
                         <?=JHTML::_('select.genericlist', PVCombo::getsFromObject($this->wards, 'ward', 'ward'), 'ward[]', 'multiple ', 'idx', 'value', (JRequest::getVar('ward') ? JRequest::getVar('ward') : ''), 'ward');?></th>
-                    <th colspan="6" data-filter="Filter by Date">&nbsp;</th>
+                    <th colspan="5" data-filter="Filter by Date">&nbsp;</th>
                 </tr>
 <?php
 endif;
