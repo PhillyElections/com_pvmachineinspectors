563d494faf16c7d9380414aabc9d6e14b4157443
matt murphy <mattyhead@gmail.com>
2016-05-31 11:01:36 -0400

    pulling in the filter-related files

---
+++ site/assets/js/filter.js
@@ -0,0 +1,57 @@
+jQuery.noConflict();
+(function($) {
+    var outer = [],
+        inner = [];
+    inner.events = function() {
+        $(document).on('click', 'a[data=all]', function() {
+            $(this).parent().find('a.mcs-item').not('.active').click();
+        });
+        $(document).on('click', 'a[data=none]', function() {
+            $(this).parent().find('a.active').click();
+        });
+        $(document).on('click', 'a[data=invert]', function() {
+            $(this).parent().find('a.mcs-item').click();
+        });
+        $(document).on('click', 'a[data=submit]', function() {
+            document.getElementById('adminForm').submit();
+        });
+    };
+    inner.build = function() {
+        var selectcontrols = document.querySelectorAll("[data-filter]"), $selectcontrol=[];
+        for (var i=0; i<selectcontrols.length; i++) {
+            $selectcontrol = $(selectcontrols[i]);
+            $selectcontrol.MultiColumnSelect({
+                multiple: true,
+                useOptionText: true,
+                hideselect: true,
+                openmenuClass: 'mcs-open',
+                openmenuText: $selectcontrol.data('filter'),
+                openclass: 'open',
+                containerClass: 'mcs-container',
+                itemClass: 'mcs-item',
+                idprefix: 'wards-',
+                duration: 200,
+                onOpen: function() {},
+                onClose: function() {},
+                onItemSelect: function() {}
+            });
+            $selectcontrol.children('.mcs-container').append('<hr class="clear" />');
+            $selectcontrol.MultiColumnSelectAddItem('all', 'All', 'wards-');
+            $selectcontrol.MultiColumnSelectAddItem('none', 'None', 'wards-');
+            $selectcontrol.MultiColumnSelectAddItem('invert', 'Invert', 'wards-');
+            $selectcontrol.MultiColumnSelectAddItem('submit', 'Submit', 'wards-');
+        }
+    };
+    outer.init = function() {
+        inner.build();
+        inner.events();
+    };
+    $(function() {
+        window.setTimeout(function() {outer.init();}, 50);
+        
+    });
+
+//    window.addEvent('domready', function() {
+
+//    });
+})(jQuery);
