392850bc1cf059f391412a607f20f2997241f9b8
matt murphy <mattyhead@gmail.com>
2016-05-31 11:08:40 -0400

    wrong loading order

--- admin/views/applicants/tmpl/default.php
+++ admin/views/applicants/tmpl/default.php
@@ -6,8 +6,8 @@ jimport("pvcombo.PVCombo");
 
 $document = &JFactory::getDocument();
 $document->addCustomTag('<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>');
-$document->addCustomTag('<script src="components/com_pvmachineinspectors/assets/js/filter.js"></script>');
 $document->addCustomTag('<script src="/media/multi-column-select/Multi-Column-Select/Multi-Column-Select.js" async defer></script>');
+$document->addCustomTag('<script src="components/com_pvmachineinspectors/assets/js/filter.js"></script>');
 $document->addStyleSheet('components/com_pvmachineinspectors/assets/css/filter.css');
 ?>
 <form action="<?=JRoute::_('index.php?option=com_pvmachineinspectors');?>" method="post" name="adminForm" id="adminForm">
